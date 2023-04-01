1df945319b3145a1caf5cfa45e0668ca4f2ae7cb
sahiltanwar15 <108778973+sahiltanwar15@users.noreply.github.com>
2023-04-01 16:27:18 +0530

    Add files via upload

---
+++ macchanger3.py
@@ -0,0 +1,18 @@
+import subprocess
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+
+
+parser.parse_args()
+
+interface = input("interface > ")
+new_mac = input("new MAC > ")
+
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+subprocess.call(["ifconfig ", interface, " down"])
+subprocess.call(["ifconfig ", interface, " hw ether", new_mac])
+subprocess.call(["ifconfig ", interface, " up"])
